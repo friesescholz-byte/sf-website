4637838a27e619c23f0c4b7f18cdf119555c76fc
Scholz und Friese <info@scholz-friese-webdesign.de>
2026-06-09 09:29:50 +0200

    Remove fallback credentials from source code entirely, migrating fully to Cloudflare env secrets

--- functions/api/send-email.js
+++ functions/api/send-email.js
@@ -21,17 +21,17 @@ export async function onRequestPost(context) {
     }
 
     // 2. Turnstile Token validieren
-    let turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY || ('0x4AAAAAA' + 'DVEq5r1ZKxsxl7Fgj3saWNB2r8');
+    let turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY;
     if (source === 'work4palace') {
-      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_WORK4PALACE || ('0x4AAAAAA' + 'dcY8na6YgQLDx0oSPZuC_DBuco');
+      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_WORK4PALACE;
     } else if (source === 'Stephan-van-Hausen') {
-      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_STEPHAN_VAN_HAUSEN || ('0x4AAAAAA' + 'DdygO8vdA6V1PJegyMQHIiJR1E');
+      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_STEPHAN_VAN_HAUSEN;
     } else if (source === 'weymann-gebaeudetechnik') {
-      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_WEYMANN_GEBAEUDETECHNIK || ('0x4AAAAAA' + 'DfUURKAuZiZYwpQqePXh_putgs');
+      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_WEYMANN_GEBAEUDETECHNIK;
     } else if (source === 'elementbau-nienburg') {
-      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_ELEMENTBAU_NIENBURG || ('0x4AAAAAA' + 'DYkqblgcNFy2414q1-0SYR3RH8');
+      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_ELEMENTBAU_NIENBURG;
     } else if (source === 'bestattungen-eberhardt') {
-      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_BESTATTUNGEN_EBERHARDT || ('0x4AAAAAA' + 'Dg25YrMjOJ1aNvVqzbVKj7kqQE');
+      turnstileSecret = env.CLOUDFLARE_TURNSTILE_SECRET_KEY_BESTATTUNGEN_EBERHARDT;
     }
     const verifyResult = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
       method: 'POST',
@@ -48,7 +48,7 @@ export async function onRequestPost(context) {
     }
 
     // 3. E-Mail bauen – je nach Quelle (source)
-    const resendApiKey = env.RESEND_API_KEY || ('re_' + '23WnEvZS_MiA7sHvE1HVkZC5TDV7TeqXi');
+    const resendApiKey = env.RESEND_API_KEY;
     let emailSubject, emailHtml, fromName;
     let recipientEmail = 'support@scholz-friese-chatbot.de';
 
